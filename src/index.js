36dd55aa53a928792b7960f2b4d949d09517327a
ljonashm <shamordinanton@gmail.com>
2021-03-30 22:32:09 +0300

    complited layout of meeter

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import './styles/style.scss';
 
-console.log(1);
+console.log(2);
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,18 @@ module.exports = {
             {
                 test: /\.(png|jpg)$/,
                 loader: 'url-loader'
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/'
+                        }
+                    }
+                ]
             }
         ]
     },
